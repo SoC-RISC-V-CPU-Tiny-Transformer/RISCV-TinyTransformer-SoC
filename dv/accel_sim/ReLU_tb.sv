c2013502c7efd07b23b048cf393003615e041664
HungCE2409 <nguyenhung84774821602@gmail.com>
2026-03-11 14:08:48 +0700

    Parallel ReLU

--- dv/accel_sim/ReLU_tb.sv
+++ dv/accel_sim/ReLU_tb.sv
@@ -2,87 +2,96 @@
 
 module ReLU_tb;
 
-  parameter int DATA_W = 8;
+  parameter int DATA_W  = 8;
+  parameter int SEQ_LEN = 4;
 
-  logic                     clk;
-  logic                     rst_n;
+  logic                                       clk;
+  logic                                       rst_n;
 
-  logic signed [DATA_W-1:0] x_in;
-  logic                     x_valid;
-  logic                     x_last;
+  logic signed [SEQ_LEN-1:0][DATA_W-1:0]      x_in;
+  logic                                       x_valid;
 
-  logic signed [DATA_W-1:0] relu_out;
-  logic                     relu_valid;
-  logic                     relu_last;
+  logic signed [SEQ_LEN-1:0][DATA_W-1:0]      relu_out;
+  logic                                       relu_valid;
 
-  ReLU #(.DATA_W(DATA_W)) dut (
+  // Instantiate DUT
+  ReLU #(
+    .DATA_W(DATA_W),
+    .SEQ_LEN(SEQ_LEN)
+  ) dut (
     .clk       (clk),
     .rst_n     (rst_n),
     .x_in      (x_in),
     .x_valid   (x_valid),
-    .x_last    (x_last),
     .relu_out  (relu_out),
-    .relu_valid(relu_valid),
-    .relu_last (relu_last)
+    .relu_valid(relu_valid)
   );
 
-  // Clock (100MHz -> Cycle 10ns) ---
   initial begin
     clk = 0;
     forever #5 clk = ~clk;
   end
-
   initial begin
     rst_n   = 0;
     x_in    = '0;
     x_valid = 0;
-    x_last  = 0;
 
-    // Wait 20ns 
+    // Wait 20ns Reset
     #20;
     rst_n = 1;
     #10;
 
+    // --- ARRAY 1 ---
+    x_in[0] = 8'sd32;  // 2.0
+    x_in[1] = -8'sd24; // -1.5
+    x_in[2] = 8'sd0;   // 0.0
+    x_in[3] = 8'sd88;  // 5.5
+    send_array(x_in);
 
-    // Data (Q3.4)
-    // Input: [ 2.0, -1.5,  0.0,  5.5, -3.0]
-    // Q3.4:  [  32,  -24,    0,   88,  -48]
+    // Next cycle, valid -> 0 to stop send 
+    @(posedge clk);
+    #1;
+    x_valid = 0;
+
+    wait(relu_valid == 1'b1);
     
-    send_data(8'sd32,  0); // +2.0
-    send_data(-8'sd24, 0); // -1.5 
-    send_data(8'sd0,   0); //  0.0 
-    send_data(8'sd88,  0); // +5.5 
-    send_data(-8'sd48, 1); // -3.0 -> x_last = 1
+    @(posedge clk); 
+    $display("-------------------------------------------------");
+
+    // --- ARRAY 2 ---
+    x_in[0] = -8'sd48; // -3.0
+    x_in[1] = 8'sd16;  // 1.0
+    x_in[2] = -8'sd8;  // -0.5
+    x_in[3] = 8'sd64;  // 4.0
+    send_array(x_in);
 
     @(posedge clk);
     #1;
     x_valid = 0;
-    x_last  = 0;
 
-    repeat(5) @(posedge clk);
+    // Wait result
+    wait(relu_valid == 1'b1);
     
+    repeat(3) @(posedge clk);
+    
+    $display("=================================================");
     $finish;
   end
 
-
-  task send_data(input logic signed [DATA_W-1:0] data, input logic is_last);
+  task send_array(input logic signed [SEQ_LEN-1:0][DATA_W-1:0] data_array);
     begin
       @(posedge clk);
       #1; 
-      x_in    = data;
+      x_in    = data_array;
       x_valid = 1;
-      x_last  = is_last;
-      
-      $display("Time %0t | INPUT  -> x_in: %4d | valid: %b | last: %b", 
-               $time, x_in, x_valid, x_last);
+      $display("Time %0t | INPUT  -> ARRAY: %p | valid: %b", $time, x_in, x_valid);
     end
   endtask
 
   always @(posedge clk) begin
     #2; 
     if (relu_valid) begin
-      $display("Time %0t | OUTPUT <- out : %4d | valid: %b | last: %b", 
-               $time, relu_out, relu_valid, relu_last);
+      $display("Time %0t | OUTPUT <- ARAY: %p | valid: %b", $time, relu_out, relu_valid);
     end
   end
 
--- rtl/accelerator/ReLU.sv
+++ rtl/accelerator/ReLU.sv
@@ -1,41 +1,31 @@
 `timescale 1ns / 1ps
 
-// ReLU activation: outputs max(0, x), sets negative values to zero
-module ReLU #(parameter int DATA_W = 8) (
-  input  logic                     clk,
-  input  logic                     rst_n,
+module ReLU #(parameter int DATA_W  = 8, parameter int SEQ_LEN = 4) (
+  input  logic                                       clk,
+  input  logic                                       rst_n,
 
-  input  logic signed [DATA_W-1:0] x_in,
-  input  logic                     x_valid, 
-  input  logic                     x_last,
+  input  logic signed [SEQ_LEN-1:0][DATA_W-1:0]      x_in,
+  input  logic                                       x_valid, 
 
-  output logic signed [DATA_W-1:0] relu_out,
-  output logic                     relu_valid,
-  output logic                     relu_last
+  output logic signed [SEQ_LEN-1:0][DATA_W-1:0]      relu_out,
+  output logic                                       relu_valid
 );
 
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      // Reset toàn bộ tín hiệu đầu ra về 0
       relu_out   <= '0;
       relu_valid <= 1'b0;
-      relu_last  <= 1'b0;
     end else begin
       relu_valid <= x_valid;
-      relu_last  <= x_last;
 
       if (x_valid) begin
-      
-        if (x_in[DATA_W-1]) begin
-          relu_out <= '0;   
-        end else begin
-          relu_out <= x_in; 
+        for (int i = 0; i < SEQ_LEN; i++) begin
+          if (x_in[i][DATA_W-1]) relu_out[i] <= '0;       // Negative -> 0
+          else                   relu_out[i] <= x_in[i];  // 
         end
-
       end else begin
-        relu_out <= '0; 
+        relu_out <= '0;
       end
-
     end
   end
 
